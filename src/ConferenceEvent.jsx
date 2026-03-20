d7ab30823343ea0c275e5e74b91d2be47725281d
Carlos <carlosdanielmurillo17@gmail.com>
2026-03-20 17:17:15 -0600

    code for add-ons

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -3,12 +3,16 @@ import "./ConferenceEvent.css";
 import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
+import { decrementAvQuantity, incrementAvQuantity } from "./avSlice";
+
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
+    const avTotalCost = calculateTotalCost("av");
 
     
     const handleToggleItems = () => {
@@ -29,9 +33,11 @@ const ConferenceEvent = () => {
         }
       };
     const handleIncrementAvQuantity = (index) => {
+      dispatch(incrementAvQuantity(index));
     };
 
     const handleDecrementAvQuantity = (index) => {
+      dispatch(decrementAvQuantity(index));
     };
 
     const handleMealSelection = (index) => {
@@ -51,6 +57,10 @@ const ConferenceEvent = () => {
         let totalCost = 0;
         if (section === "venue") {
           venueItems.forEach((item) => {
+            totalCost += item.cost * item.quantity;
+          });
+        } else if (section === "av"){
+          avItems.forEach((item) => {
             totalCost += item.cost * item.quantity;
           });
         }
@@ -144,6 +154,21 @@ const ConferenceEvent = () => {
             </div>
           ))}
         </div>
+        {avItems.map((item, index) => (
+          <div className="av_data venue_main" key={index}>
+            <div className="img">
+              <img src={item.img} alt={item.name} />
+            </div>
+
+            <div className="text"> {item.name} </div>
+            <div> ${item.cost} </div>
+              <div className="addons_btn">
+                <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                <span className="quantity-value"> {item.quantity} </span>
+                <button className="btn-success" onClick={() => handleIncrementAvQuantity}> &#43; </button>
+              </div>
+          </div>
+        ))}
         <div className="total_cost">Total Cost: ${venueTotalCost}</div>
       </div>
 
@@ -159,7 +184,7 @@ const ConferenceEvent = () => {
                                 <div className="addons_selection">
 
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: {avTotalCost}</div>
 
                             </div>
 
--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,51 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+      img: "https://unsplash.com/photos/black-and-white-benq-projector-EEnJpGHkU4k",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+    {
+      img: "https://unsplash.com/photos/black-and-brown-bookshelf-speaker-on-black-surface-u8-QI4tRES0",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0,
+    },
+    {
+      img: "https://unsplash.com/photos/black-and-gray-corded-microphone-hvgd0ygXuQQ",
+      name: "Microphone",
+      cost: 45,
+      quantity: 0,
+    },
+    {
+      img: "https://unsplash.com/photos/a-white-board-sitting-on-top-of-a-wooden-floor-FYFKBiWLq88",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+    {
+      img: "https://unsplash.com/photos/text-pAoOSyIHoNA",
+      name: "signage",
+      cost: 80,
+      quantity: 0,
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+      const item = state[action.payload];
+      if (item && item.quantity > 0) {
+        item.quantity--;
+      } 
     },
   },
 });
